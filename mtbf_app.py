6e78e2e65ea4ce09e6250897864daf5223c9c864
luis3809 <luicord@gmail.com>
2026-06-04 22:43:16 -0500

    Cambios en requerimientos.txt y mtbf_app.py para eliminar la importación de plotly.express, ya que no se está utilizando en el código. Esto ayudará a reducir las dependencias y mejorar el rendimiento de la aplicación.

--- mtbf_app.py
+++ mtbf_app.py
@@ -1,7 +1,7 @@
 import streamlit as st
 import pandas as pd
-import plotly.express as px
-
+#import plotly.express as px
+st.write("Plotly test")
 # ----------------------------------
 # CONFIG
 # ----------------------------------
